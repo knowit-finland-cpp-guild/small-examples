24925bf594ca66b16ea347ed1904228968a290c2
Juhani Dabek <juhani.dabek@gmail.com>
2023-07-20 22:16:31 +0300

    operator< right and wrong implementation

---
+++ operator_less_impl.cpp
@@ -0,0 +1,267 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+#include <signal.h>
+
+/****************************************************
+
+* C++ code illustrating:
+
+* Incorrect implementation of operator<
+    - Right implementation works when called
+    - Wrong implementation fails when called
+    - Fail occurs at sorting of vector with keys with
+      wrong operator< (KeyWrong)
+* Operator selection is based on structs inherited
+  from parent (KeyBase)
+* Insertion/printing/sorting of elements in vector
+  based on a function pointer
+    - Selected based on usage of KeyRight or KeyWrong
+* Simple argument parsing from command line
+* Simple handling of a segfault
+
+****************************************************/
+
+// Enumeration for right and wrong keys
+enum class Comparison { RIGHT, WRONG };
+
+class KeyRight;
+class KeyWrong;
+
+// Count of increments in key's 1st, 2nd and 3rd elements
+int aSteps = 13;
+int bSteps = 5;
+int cSteps = 17;
+
+// Key data type in scalar form
+using Value = double;
+
+// Multipliers for conversion from 3 elements to scalar form
+Value aMultiply = 1.0/aSteps;
+Value bMultiply = aMultiply/bSteps;
+Value cMultiply = bMultiply/cSteps;
+
+// How many keys are to be placed in vector
+int nSamp = 30;
+
+// Base struct for keys
+struct KeyBase
+{
+public:
+    KeyBase(const int a, const int b, const int c) :
+        a(a), b(b), c(c), val(a*aMultiply+b*bMultiply+c*cMultiply)
+    {}
+
+    virtual bool operator<(const KeyBase& rhs) const = 0;
+
+    friend std::ostream& operator<<(
+            std::ostream& os, const KeyBase& key)
+    {
+        return os << "(" << key.getA() << ", " << key.getB() << ", "
+                  <<        key.getC() << ") " << key.val;
+    }
+
+    int getA() const { return a; }
+    int getB() const { return b; }
+    int getC() const { return c; }
+
+protected:
+    int a;      // Element 1 in key
+    int b;      // Element 2 in key
+    int c;      // Element 3 in key
+    Value val;  // Key in scalar form
+};
+
+// Descendant of base with right implementation of operator<
+struct KeyRight : public KeyBase
+{
+public:
+    KeyRight(const int a, const int b, const int c) :
+        KeyBase(a, b, c)
+    {}
+
+    bool operator<(const KeyBase& rhs) const override
+    {
+        return a == rhs.getA() ?
+               b == rhs.getB() ?
+               c <  rhs.getC() :
+               b <  rhs.getB() :
+               a <  rhs.getA();
+    }
+};
+
+// Descendant of base with wrong implementation of operator<
+struct KeyWrong : public KeyBase
+{
+public:
+    KeyWrong(const int a, const int b, const int c) :
+        KeyBase(a, b, c)
+    {}
+
+    bool operator<(const KeyBase& rhs) const override
+    {
+        if (a < rhs.getA())
+            return true;
+        if (b < rhs.getB())
+            return true;
+        if (c < rhs.getC())
+            return true;
+        return false;
+    }
+};
+
+// Holder class for vectors with either right or wrong operator<
+class VecKey
+{
+public:
+    VecKey(const Comparison comp) : comp(comp)
+    {
+        if (comp == Comparison::RIGHT)
+        {
+            // Select which function pointers to use with right keys
+            insertFunction = &VecKey::insertRight;
+            printFunction = &VecKey::printRight;
+            sortFunction = &VecKey::sortRight;
+        }
+        else
+        {
+            // Select which function pointers to use with wrong keys
+            insertFunction = &VecKey::insertWrong;
+            printFunction = &VecKey::printWrong;
+            sortFunction = &VecKey::sortWrong;
+        }
+    }
+
+    void insert(int a, int b, int c)
+    {
+        (this->*insertFunction)(a, b, c);
+    }
+
+    void print() const
+    {
+        (this->*printFunction)();
+    }
+
+    void sort()
+    {
+        (this->*sortFunction)();
+    }
+
+private:
+    VecKey();
+
+    void insertRight(int a, int b, int c)
+    {
+        vRight.emplace_back(a, b, c);
+    }
+
+    void insertWrong(int a, int b, int c)
+    {
+        vWrong.emplace_back(a, b, c);
+    }
+
+    void printRight() const
+    {
+        int n = 0;
+        for (const auto& x : vRight)
+            std::cout << ++n << ": " << x << std::endl;
+    }
+
+    void printWrong() const
+    {
+        int n = 0;
+        for (const auto& x : vWrong)
+            std::cout << ++n << ": " << x << std::endl;
+    }
+
+    void sortRight()
+    {
+        std::sort(vRight.begin(), vRight.end());
+    }
+
+    void sortWrong()
+    {
+        std::sort(vWrong.begin(), vWrong.end());
+    }
+
+    Comparison comp;
+    std::vector<KeyRight> vRight;
+    std::vector<KeyWrong> vWrong;
+    void (VecKey::*insertFunction)(int, int, int);
+    void (VecKey::*printFunction)() const;
+    void (VecKey::*sortFunction)();
+};
+
+// Determine the desired run, right or wrong
+bool parseArgs(const int argc, char *argv[], Comparison& comp)
+{
+    auto argRight = std::make_pair("comparison-right", false);
+    auto argWrong = std::make_pair("comparison-wrong", false);
+
+    for (int n = 1; n < argc; ++n)
+    {
+        if (std::string(argv[n]) == argRight.first)
+            argRight.second = true;
+        else if (std::string(argv[n]) == argWrong.first)
+            argWrong.second = true;
+    }
+
+
+    if (argc != 2 || argRight.second == argWrong.second)
+    {
+        std::cout << "Incompatible arguments. Use examples:"
+                  << std::endl;
+        std::cout << argv[0] << " " << argRight.first << std::endl;
+        std::cout << argv[0] << " " << argWrong.first << std::endl;
+        return false;
+    }
+
+    comp = Comparison::RIGHT;
+    if (argWrong.second)
+        comp = Comparison::WRONG;
+
+    return true;
+}
+
+// Handle possible segfault in sorting vector
+void handler(int sig)
+{
+    std::cout << "Segmentation fault: " << sig << std::endl;
+
+    exit(sig);
+}
+
+int main(int argc, char *argv[])
+{
+    // Prepare for possible segfault
+    signal(SIGSEGV, handler);
+
+    Comparison comp;
+
+    // Determine the desired run
+    if (!parseArgs(argc, argv, comp))
+        return 1;
+
+    // Initialize vector holder class for either right or wrong key
+    VecKey vk(comp);
+
+    std::cout << "Inserting" << std::endl;
+    // Populate the right or wrong vector in the vector holder class
+    for (int n = 1; n <= nSamp; ++n)
+    {
+        int a = n % aSteps;
+        int b = n % bSteps;
+        int c = n % cSteps;
+        vk.insert(a, b, c);
+    }
+    // Print the right or wrong vector in initial order
+    vk.print();
+
+    std::cout << "Sorting" << std::endl;
+    // Sort the right or wrong vector
+    vk.sort();
+    // Print the right or wrong vector in sorted order
+    vk.print();
+}
+
